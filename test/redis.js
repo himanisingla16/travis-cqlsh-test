d7fe48a422562517cf5993de268b428d49557302
Himani <himani.singla@niit.com>
2017-08-21 11:23:14 +0530

    Added redis test cases

---
+++ test/redis.js
@@ -0,0 +1,24 @@
+const redis = require('thunk-redis');
+const client = redis.createClient({database: 1, usePromise: true});
+const chai = require('chai');
+chai.should();
+
+describe('Redis Travis Test Case', function() {
+
+  before(function(done) {
+  	client.on('connect', done);
+  });
+
+  it('should create list b', function(done) {
+  	client.set('z', '20').then(function() {
+  		done();
+  	});
+  });
+
+  it('should retrieve data from list b', function(done) {
+  	client.get('z').then(function(reply) {
+  	  reply.should.be.equal('20');
+  	  done();
+  	});
+  });
+});
--- test/test.js
+++
@@ -1,5 +0,0 @@
-describe('Passing Test Case', function() {
-	it('should pass', function(done) {
-        done();
-	});
-});
