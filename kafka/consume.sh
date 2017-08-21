80e0a12a9146dd7095f2f1d2161e9ebb4acca45b
Himani <himani.singla@niit.com>
2017-08-21 10:59:30 +0530

    added file path

--- kafka/consume.sh
+++ kafka/consume.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -e
-bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic dev_activities --max-messages 1 --from-beginning
+/opt/kafka_2.11-0.10.1.0/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic dev_activities --max-messages 1 --from-beginning
