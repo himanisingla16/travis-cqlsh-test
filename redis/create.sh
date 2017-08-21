edd6fea6a170bfd852064d52219fd71c6e630bd0
Himani <himani.singla@niit.com>
2017-08-21 10:48:48 +0530

    Removed docker reference from shell scripts

--- redis/create.sh
+++ redis/create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -e
-docker exec -it traviscqlshtest_redis_1 redis-cli lpush a 3
+redis-cli lpush a 3
--- redis/retrieve.sh
+++ redis/retrieve.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -e
-docker exec -it traviscqlshtest_redis_1 redis-cli lrange a 0 -1
+redis-cli lrange a 0 -1
